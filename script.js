f20efce64db9c34b358656751d4d348484f19819
Hilan Eduardo Alegria Yate <hilan.alegria00@usc.edu.co>
2025-08-17 12:07:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,51 @@ document.addEventListener("DOMContentLoaded", () => {
 
   elements.forEach((el) => observer.observe(el));
 });
+
+
+const canvas = document.getElementById("matrix");
+
+if (canvas) {
+  const ctx = canvas.getContext("2d");
+
+  function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = document.querySelector("header").offsetHeight;
+  }
+
+  resizeCanvas();
+  const letters = "アカサタナハマヤラワ0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const matrix = letters.split("");
+  const fontSize = 16;
+  let columns = canvas.width / fontSize;
+  const drops = [];
+  function resetDrops() {
+    columns = Math.floor(canvas.width / fontSize);
+    for (let x = 0; x < columns; x++) {
+      drops[x] = 1;
+    }
+  }
+
+  resetDrops();
+
+  function draw() {
+    ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "#ff7e00";
+    ctx.font = fontSize + "px monospace";
+    for (let i = 0; i < drops.length; i++) {
+      const text = matrix[Math.floor(Math.random() * matrix.length)];
+      ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+
+      if (drops[i] * fontSize > canvas.height && Math.random() > 0.975) {
+        drops[i] = 0;
+      }
+      drops[i]++;
+    }
+  }
+  setInterval(draw, 33);
+  window.addEventListener("resize", () => {
+    resizeCanvas();
+    resetDrops();
+  });
+}
